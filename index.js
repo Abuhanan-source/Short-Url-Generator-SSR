2da3373d8df114a10805e5d650e73d5c92d57b38
Abuhanan-source <hh5493483@gmail.com>
2026-07-05 15:46:03 +0500

    Enable trust proxy in Express app configuration

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ dotenv.config();
 
 const app = express();
 const PORT = 3000;
+app.set('trust proxy', true); 
 app.use(express.json());  
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
